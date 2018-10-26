526e7af7be6c348a2a8f1e0b0ee6a30dae912452
Greg3380 <grzegorz.wator10@gmail.com>
2018-10-26 19:31:14 +0200

    code cleanup

--- app/src/main/java/bookingsystem/agh/edu/bookingapp/activity/OverviewActivity.java
+++ app/src/main/java/bookingsystem/agh/edu/bookingapp/activity/OverviewActivity.java
@@ -28,14 +28,14 @@ public class OverviewActivity extends AppCompatActivity {
             }
         });
 
-        CardView showRestaurantListButton = findViewById(R.id.showMRestaurantListButton);
-        showRestaurantListButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), RestaurantListActivity.class);
-                view.getContext().startActivity(intent);
-            }
-        });
+//        CardView showRestaurantListButton = findViewById(R.id.showMRestaurantListButton);
+//        showRestaurantListButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(view.getContext(), RestaurantListActivity.class);
+//                view.getContext().startActivity(intent);
+//            }
+//        });
 
         CardView showMyReservations = findViewById(R.id.show_my_reservations);
         showMyReservations.setOnClickListener(new View.OnClickListener() {
