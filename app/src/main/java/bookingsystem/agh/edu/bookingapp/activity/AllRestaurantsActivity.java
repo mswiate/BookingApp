f817bbfa0a1fd30620eaaa2b65f049c865935df3
mswiate <marekswiatek1996@gmail.com>
2018-11-03 10:55:04 +0100

    add nav menu to restaurants activity

--- app/src/main/java/bookingsystem/agh/edu/bookingapp/activity/AllRestaurantsActivity.java
+++ app/src/main/java/bookingsystem/agh/edu/bookingapp/activity/AllRestaurantsActivity.java
@@ -10,12 +10,13 @@ import android.widget.ListView;
 import java.util.List;
 
 import bookingsystem.agh.edu.bookingapp.R;
+import bookingsystem.agh.edu.bookingapp.activity.tools.ActivityWithMenu;
 import bookingsystem.agh.edu.bookingapp.adapter.AllRestaurantsAdapter;
 import bookingsystem.agh.edu.bookingapp.controls.FiltersView;
 import bookingsystem.agh.edu.bookingapp.model.Restaurant;
 import bookingsystem.agh.edu.bookingapp.task.GetRestaurantsTask;
 
-public class AllRestaurantsActivity extends AppCompatActivity {
+public class AllRestaurantsActivity extends ActivityWithMenu {
 
     private ListView listView;
 
@@ -26,8 +27,8 @@ public class AllRestaurantsActivity extends AppCompatActivity {
 
         this.listView = findViewById(R.id.all_restaurants_listview);
 
-        Button testButton = findViewById(R.id.all_restaurants_filters);
-        testButton.setOnClickListener(new View.OnClickListener() {
+        Button filterButton = findViewById(R.id.all_restaurants_filters);
+        filterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 BottomSheetDialog dialog = new BottomSheetDialog(AllRestaurantsActivity.this);
@@ -48,6 +49,8 @@ public class AllRestaurantsActivity extends AppCompatActivity {
                 .context(this)
                 .build()
                 .execute();
+
+        prepareNavigationMenu(R.id.restaurantsDrawerLayout, R.id.restaruants_nav_view, this);
     }
 
 
